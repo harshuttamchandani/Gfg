04a9ac8c4f4911c426707dd15be4d675246b9ab4
Harsh Uttamchandani <harshuttamchandani2002@gmail.com>
2023-12-23 19:21:48 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Red OR Green/red-or-green.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+       int RedOrGreen(int N,string S) {
+           //code here
+           int r=0;
+           int g=0;
+           for(int i=0;i<N;i++)
+           {
+               if(S[i]=='R')
+               {
+                   r++;
+               }
+               else
+               {
+                   g++;
+               }
+               
+               
+           }
+           if (r>=g)
+           {
+               return g;
+           }
+           else
+           {
+               return r;
+           }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.RedOrGreen(N,S) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
