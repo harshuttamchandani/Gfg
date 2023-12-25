91b0f3fc9afa6355a748b42bc05335f97ef9e4d5
Harsh Uttamchandani <harshuttamchandani2002@gmail.com>
2023-12-25 17:20:37 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count More than n/k Occurences/count-more-than-nk-occurences.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// A C++ program to print elements with count more than n/k
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find all elements in array that appear more than n/k times.
+    int countOccurence(int arr[], int n, int k) {
+        // Your code here
+        sort(arr,arr+n);
+        int i=0;
+          int j=1;
+          int count =1;
+          int ans =0;
+          while(j<n){
+              
+              if(arr[i]==arr[j]){
+                  j++;
+                  count ++;
+              }
+            
+            if (arr[i] != arr[j] || j == n) {
+                if (count > (n / k)) {
+                ans++; }
+           
+                i = j;
+                 j++;
+                count = 1; 
+              
+          }
+              
+              
+              
+              
+              
+          }
+          
+          
+          return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+
+        int arr[n];
+
+        for (i = 0; i < n; i++) cin >> arr[i];
+        int k;
+        cin >> k;
+        Solution obj;
+        cout << obj.countOccurence(arr, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
